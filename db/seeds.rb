a80a3754a66f5a8c8925d203388289c1a791c80f
Carey Johnson <johnson.careyj@gmail.com>
2018-06-10 14:54:46 -0700

    Added seed to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "carey1", email: "email1",  password: "password");
