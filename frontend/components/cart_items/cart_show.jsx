96af2160a4a08916d0e593f27fcb99d3e5ee1bbd
Carey Johnson <johnson.careyj@gmail.com>
2018-07-06 02:33:45 -0700

    Added totaling functionality in the cart. Adds up total price in users cart

--- frontend/components/cart_items/cart_show.jsx
+++ frontend/components/cart_items/cart_show.jsx
@@ -8,33 +8,47 @@ class CartShow extends React.Component {
     
     this.state = {
       total: 0
-    }
-    this.calculateTotal = this.calculateTotal.bind(this);
+    };
+
+    // this.calculateTotal = this.calculateTotal.bind(this);
+    // this.updateTotal = this.updateTotal.bind(this);
   }
 
   componentDidMount() {
+   
     this.props.fetchCartItems();
+    // this.calculateTotal()
+
   }
 
 
-  calculateTotal() {
-   const totals = this.props.cartItems.map(cartItem => {
-      return cartItem.price * cartItem.quality
-    });
-   return totals;
-  }
+  // calculateTotal() {
+  //  const totals = this.props.cartItems.map(cartItem => {
+  //    console.log(cartItem.price);
+  //     return cartItem.price * cartItem.quantity;
+      
+  //   });
+  //   const reducer = (acc, currentValue) => acc + currentValue;
+  //   let total = totals.reduce(reducer);
+  //   return total;
+  // }
 
-  updateTotal() {
-    this.setState({total: this.calculateTotal()});
-  }
+  // updateTotal() {
+  //   this.setState({total: this.calculateTotal()});
+  // }
   
   render() {
-    
+      let totalSum = 0;
+      this.props.cartItems.forEach(cartItem => {
+       totalSum += (cartItem.price * cartItem.quantity);
+     });
     return (
       <div className="cartItems-box">
         <div className="header-box">
         <header className="cart-header">
         <h1>{this.props.cartItems.length} item(s) in your cart</h1>
+        <h1>${totalSum} is your total </h1>
+      
         
         </header>
         <div>
