dce2fd8294082117d612d6a23691f3e51b7a9a72
Carey Johnson <johnson.careyj@gmail.com>
2018-07-11 14:03:04 -0700

    Changed the ratings radio-button input, accurately displays new rating with review

--- frontend/components/review/review_form.jsx
+++ frontend/components/review/review_form.jsx
@@ -29,24 +29,52 @@ class ReviewForm extends React.Component {
     });
   }
 
+  handleRating(e) {
+    this.setState({ rating: e.currentTarget.value });
+  }
 
   render() {
     console.log(this.state.body);
     return (
       <div>
         <form onSubmit={this.handleSubmit} id="reviewform">
-        <label htmlFor="">Leave Review:
+        <label htmlFor="">Rate this product:
+          {/* <select className="product-rating" value={this.state.rating}
+              className="dropdown"
+              onChange={this.handleRating.bind(this)}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select> */}
+            <br/>
+            5 
+            <input type="radio" name="rating" onClick={this.handleRating.bind(this)} value="5"/>
+            <br/>
+            4
+            <input type="radio" name="rating" onClick={this.handleRating.bind(this)} value="4"/>
+            <br/>
+            3
+            <input type="radio" name="rating" onClick={this.handleRating.bind(this)} value="3"/>
+            <br/>
+            2
+            <input type="radio" name="rating" onClick={this.handleRating.bind(this)} value="2"/>
+            <br/>
+            1
+            <input type="radio" name="rating" onClick={this.handleRating.bind(this)} value="1"/>
+            <br/>
         </label>
         <br/>
         </form>
-        <textarea className="textarea" rows="4" cols="50" onChange={this.handleText()} name="comment" form="reviewform">
-          Enter review here...
+        <textarea className="textarea" rows="4" cols="50" onChange={this.handleText()} 
+        name="comment" placeholder="Enter Review Here" form="reviewform" autofocus>
+          
           </textarea>
         <input className="review-submit" type="submit" onClick={this.handleSubmit} value="submit"/>
       </div>
     );
   }
-
 
 }
 
