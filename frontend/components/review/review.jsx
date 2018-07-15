1ae5318a764d0102a2f4f16850b87899b3284b9f
Carey Johnson <johnson.careyj@gmail.com>
2018-07-14 20:44:31 -0700

    Added gold star ratings to the reviews

--- frontend/components/review/review.jsx
+++ frontend/components/review/review.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Review = ({review, deleteReview}) => {
   let today = new Date();
   let dd = today.getDate();
-  let mm = today.getMonth() + 1; //January is 0!
+  let mm = today.getMonth() + 1;
   let yyyy = today.getFullYear();
 
   if (dd < 10) {
@@ -14,25 +14,52 @@ export const Review = ({review, deleteReview}) => {
     mm = '0' + mm;
   }
   today = mm + '/' + dd + '/' + yyyy;
+
+  let one = <span className="stars">&#9733;</span>;
+  let two = <span className="stars">&#9733;&#9733;</span>;
+  let three = <span className="stars">&#9733;&#9733;&#9733;</span>;
+  let four = <span className="stars">&#9733;&#9733;&#9733;&#9733;</span>;
+  let five = <span className="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>;
+  let stars;
+
+  if (review.rating === 1) {
+    stars = one;
+  } else if (review.rating === 2) {
+    stars = two;
+  } else if (review.rating === 3) {
+    stars = three;
+  } else if (review.rating === 4) {
+    stars = four;
+  } else {
+    stars = five;
+  }
+
+
   return (
    
-
+    <div>
     <div className="singlereview-box">
       <div className="leftside-boxreview">
       {review.author_name} 
       </div>
       
       <div className="rightside-boxreview">
-        <div>
-        rating: {review.rating}
-        date: {today}
+        <div className="review-top">
+          <div>
+              {stars}
+          </div>
+          <div>
+          {today}
+          </div>
         </div>
         <div className="review-body">
         {review.body}
         </div>
+      <button className="review-delete" onClick={() => deleteReview(review.id)}>delete</button>
       </div>
       <br/>
-      <button className="review-delete" onClick={() => deleteReview(review.id)}>delete</button>
     </div>
+    </div>
+
   );
 };
--- frontend/components/review/reviews_index.jsx
+++ frontend/components/review/reviews_index.jsx
@@ -14,6 +14,7 @@ class ReviewsIndex extends React.Component {
           {/* <ReviewForm product={this.props.product} createReview={this.props.createReview} userId={this.props.userId}
           username={this.props.username} /> */}
         <ul>
+          <h1 className="reviews-h1">Reviews</h1>
           <li>
             {this.props.product.reviews.map(review => 
             <Review review={review} key={review.id} deleteReview={this.props.deleteReview}/>)}
