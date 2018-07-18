41ad8495e91bb1af547a18fdd040eca4e842e21f
Carey Johnson <johnson.careyj@gmail.com>
2018-07-18 11:09:41 -0700

    Added links to the category page for each nav bar element

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom';
 
 const  Navbar = () => {
   return (
     <nav className="navbar">
       <ul className="nav-list">
-        <li>Collectables</li>
-        <li>Pets</li>
-        <li>Food</li>
-        <li>Clothes</li>
-        <li>Jewelry</li>
-        <li>Vehicles</li>
-        <li>Household</li>
-        <li>Electronics</li>
+        <Link to='/catergories/1'> <li>Entertainment</li> </Link>
+        <Link to='/catergories/2'><li>Pets</li></Link>
+        <Link to='/catergories/3'><li>Food</li></Link>
+        <Link to='/catergories/4'><li>Clothes</li></Link>
+        <Link to='/catergories/5'><li>Jewelry</li></Link>
+        <Link to='/catergories/6'><li>Vehicles</li></Link>
+        <Link to='/catergories/7'><li>Household</li></Link>
+        <Link to='/catergories/8'><li>Electronics</li></Link>
       </ul>
     </nav>
   );
