abc82e356877da8ff693f0f74f7701ab101575f4
Carey Johnson <johnson.careyj@gmail.com>
2018-07-18 21:18:23 -0700

    added style and fixed sizing/spacing for the category page

--- frontend/components/category/category_show_container.jsx
+++ frontend/components/category/category_show_container.jsx
@@ -24,11 +24,11 @@ class CategoryShowContainer extends React.Component {
     const filtered = this.props.products.filter(el =>
       el.category_name === this.props.category_id);
     return (
-      <div>
-        <ul>
+      <div className="category-box">
+        <div>
           {filtered.map(product =>
             <ProductIndexItem product={product} key={product.id} />)}
-        </ul>
+        </div>
       </div>
     );
   }
