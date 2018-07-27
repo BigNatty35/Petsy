00652c5e9a1ba749b1ea1fbd23f425a1bd0b66a0
Carey Johnson <johnson.careyj@gmail.com>
2018-07-26 17:43:47 -0700

    Added a link to the index page on the home splash page. Also added underline on hover

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -14,6 +14,8 @@ import ProductDetailContainer from './product/product_detail_container';
 import CartShowContainer from './cart_items/cart_show_container';
 import CategoryShowContainer from './category/category_show_container';
 import SearchShowContainer from './searchbar/search_show';
+import ProductIndexShowContainer from './product/product_index_show';
+
 const App = () => (
   <div className="whole-app">
     <Modal/>
@@ -31,7 +33,7 @@ const App = () => (
       <Route path='/product/:product_id' component={ProductDetailContainer}/>
       <Route path="/categories/:category_id" component={CategoryShowContainer}/>
       <Route path="/search" component={SearchShowContainer}/>
-      
+      <Route path="/products" component={ProductIndexShowContainer}/>      
       </Switch>
 
    
