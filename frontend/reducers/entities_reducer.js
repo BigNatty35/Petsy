93ff1aed36b1e55b2797cd2f5025ab63b2f716b3
Carey Johnson <johnson.careyj@gmail.com>
2018-08-04 10:17:01 -0700

    Added new comments to the entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -4,6 +4,7 @@ import productsReducer from "./products_reducer";
 import categoryReducer from "./category_reducer";
 import cartItemsReducer from './cart_items_reducer';
 
+// This is where the application state is shaped
 const entitiesReducer = combineReducers({
   users: usersReducer,
   products: productsReducer,
